c991771a5cba09eb7c641987b6703fd3d10b8e50
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-08-06 11:19:55 +0500

    feat: dynamic color theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
-import { HomeOutlined, PlusCircleOutlined, ShopOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import { Link, useNavigate } from "react-router-dom";
-import { DownOutlined } from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Dropdown, Space } from "antd";
+import { Dropdown, Space, ColorPicker, Button, Spin, MenuProps, Divider } from "antd";
+import { HomeOutlined, PlusCircleOutlined, ShopOutlined, DownOutlined } from "@ant-design/icons";
+
 import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 import { BASE_URL } from "./../utils/urls";
-import { Spin } from "antd";
+import { useTheme } from "../contexts/ThemeContext";
 
 export default function Navbar() {
     const [categories, setCategoies] = useState<MenuProps["items"]>([{ label: "Loading", key: 1 }]);
     const [loading, setLoading] = useState<boolean>(false);
 
     const navigate = useNavigate();
+
+    const { primaryColor, setPrimaryColor } = useTheme();
 
     useEffect(() => {
         fetchCategories();
@@ -39,45 +40,57 @@ export default function Navbar() {
     };
 
     return (
-        <div className="w-full flex justify-between px-6 py-4 border-emerald-300 border-b-2 mb-10">
-            <Link to="/home" className="flex gap-2 items-center">
-                <ShopOutlined className="text-2xl" />
-                <h2 className="text-2xl font-medium">Point of Sale</h2>
-            </Link>
-            <ul className="flex gap-4">
-                <Link to="/home" className="flex gap-2 items-center hover:text-emerald-500">
-                    <HomeOutlined className="text-xl" />
-                    <li className="text-xl font-medium">Home</li>
+        <>
+            <div className={`w-full flex justify-between px-6 pt-4`}>
+                <Link to="/home" className="flex gap-2 items-center">
+                    <ShopOutlined className="text-2xl" />
+                    <h2 className="text-2xl font-medium">Point of Sale</h2>
                 </Link>
-                <div className="flex gap-2 items-center">
-                    <Spin spinning={loading}>
-                        <Dropdown
-                            menu={{ items: categories }}
-                            className="hover:text-emerald-500"
-                            trigger={["click"]}
-                        >
-                            <div className="cursor-pointer">
-                                <Space className="text-xl font-medium" color="red">
-                                    Categories
-                                    <DownOutlined />
-                                </Space>
-                            </div>
-                        </Dropdown>
-                    </Spin>
-                </div>
-            </ul>
-            <ul>
-                <li>
-                    <Button
-                        type="primary"
-                        size="large"
-                        icon={<PlusCircleOutlined />}
-                        onClick={() => navigate("/products/submit")}
+                <ul className="flex gap-4">
+                    <Link
+                        to="/home"
+                        className={`flex gap-2 items-center hover:text-[${primaryColor}]`}
                     >
-                        Add
-                    </Button>
-                </li>
-            </ul>
-        </div>
+                        <HomeOutlined className="text-xl" />
+                        <li className="text-xl font-medium">Home</li>
+                    </Link>
+                    <div className="flex gap-2 items-center">
+                        <Spin spinning={loading}>
+                            <Dropdown
+                                menu={{ items: categories }}
+                                className={`hover:text-[${primaryColor}]`}
+                                trigger={["click"]}
+                            >
+                                <div className="cursor-pointer">
+                                    <Space className="text-xl font-medium">
+                                        Categories
+                                        <DownOutlined />
+                                    </Space>
+                                </div>
+                            </Dropdown>
+                        </Spin>
+                    </div>
+                </ul>
+                <ul className="flex gap-2 items-center">
+                    <li>
+                        <Button
+                            type="primary"
+                            size="large"
+                            icon={<PlusCircleOutlined />}
+                            onClick={() => navigate("/products/submit")}
+                        >
+                            Add
+                        </Button>
+                    </li>
+                    <li>
+                        <ColorPicker
+                            defaultValue="#10b981"
+                            onChange={(_, hex) => setPrimaryColor(hex)}
+                        />
+                    </li>
+                </ul>
+            </div>
+            <Divider style={{ borderColor: primaryColor }} className="mb-10" />
+        </>
     );
 }
